be1b31d11adc4836643188a2a03604bf74506f19
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2013-08-09 22:20:49 +0200

    bugs in generator

--- generator.ml
+++ generator.ml
@@ -59,11 +59,11 @@ let generate, generate_tfold, generate_novar =
 	    if fold_state = Required then
 	      begin
 		let acc = ref [] in
-		for i = 1 to size-3 do
-		  for j = 1 to size-2-i do
+		for i = 1 to size-4 do
+		  for j = 1 to size-3-i do
 		    let gen1 = aux i Forbidden in
 		    let gen2 = aux j Inside in
-		    let gen3 = aux (size-1-i-j) Forbidden in
+		    let gen3 = aux (size-2-i-j) Forbidden in
 		    acc:=
 		      List.fold_left (fun acc x ->
 			List.fold_left (fun acc y ->
@@ -88,7 +88,7 @@ let generate, generate_tfold, generate_novar =
 		if fold_state = Required then
 		  begin
 		    let acc = ref acc in
-		    for i = 1 to size-5 do
+		    for i = 5 to size-2 do
 		      let genl = aux i Required in
 		      let genr = aux (size-1-i) Forbidden in
 		      acc:=
@@ -119,7 +119,7 @@ let generate, generate_tfold, generate_novar =
 		if fold_state = Required then
 		  begin
 		    let acc = ref acc in
-		    for i = 4 to size-3 do
+		    for i = 5 to size-3 do
 		      for j = 1 to size-2-i do
 			let gen1 = aux i Required in
 			let gen2 = aux j Forbidden in
@@ -132,8 +132,8 @@ let generate, generate_tfold, generate_novar =
 			    !acc gen1
 		      done;
 		    done;
-		    for i = 1 to size-6 do
-		      for j = 4 to size-2-i do
+		    for i = 1 to size-7 do
+		      for j = 5 to size-2-i do
 			let gen1 = aux i Forbidden in
 			let gen2 = aux j Required in
 			let gen3 = aux (size-1-i-j) Forbidden in
@@ -145,8 +145,8 @@ let generate, generate_tfold, generate_novar =
 			    !acc gen1
 		      done;
 		    done;
-		    for i = 1 to size-6 do
-		      for j = 1 to size-5-i do
+		    for i = 1 to size-7 do
+		      for j = 1 to size-6-i do
 			let gen1 = aux i Forbidden in
 			let gen2 = aux j Forbidden in
 			let gen3 = aux (size-1-i-j) Required in
@@ -199,16 +199,17 @@ let generate, generate_tfold, generate_novar =
       if has_fold then List.rev_append (aux size Required) lst else lst
   in
   (fun ?(filter=true) size ?(exact=true) ops ->
-    (generate filter size exact ops [C0;C1;Var Constants.arg])),
+    generate filter (size-1) exact ops [C0;C1;Var Constants.arg]),
   (fun ?(filter=true) size ?(exact=true) ops ->
     let ops = OSet.remove Foldo ops in
-    let size = size-3 in
+    let size = size-5 in
     let lst =
       generate filter size exact ops
 	[Var Constants.fold_acc;Var Constants.fold_arg;C0;C1]
     in
     List.rev_map (fun t -> Fold (Var Constants.arg, C0, t)) lst),
-  (fun ?(filter=true) size ?(exact=true) ops -> generate filter size exact ops [C0;C1])
+  (fun ?(filter=true) size ?(exact=true) ops ->
+    generate filter (size-1) exact ops [C0;C1])
 
 let generate_constants ?(filter=true) size ?(exact=true) ops =
   List.rev_map (fun t -> eval t 0L) (generate_novar ~filter ~exact size ops)
