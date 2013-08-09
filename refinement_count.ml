8877f2e465a3b5e9facb3f01857fdcfa69d5fea1
Thibaut Balabonski <thibaut.balabonski@inria.fr>
2013-08-09 15:43:51 +0200

    Basic measurement of refinement.

---
+++ refinement_count.ml
@@ -0,0 +1,28 @@
+open Term
+open Int64
+
+let refinement_measure (terms: exp array) (selected_terms: Bitv.t) (disc_value: int64) =
+  let answers = Hashtbl.create 1021 in
+  Bitv.iteri_true
+    (fun i ->
+      let ans = Term.eval terms.(i) disc_value in
+      try
+	Hashtbl.replace answers ans (1 + Hashtbl.find answers ans)
+      with
+      | Not_found -> Hashtbl.add answers ans 1
+    ) selected_terms;
+  let max_count =
+    Hashtbl.fold
+      (fun key count max_count -> max count max_count)
+      answers 0
+  in
+  Printf.printf "Max: %d\n" max_count;
+  Printf.printf "Nombre: %d\n" (Bitv.length selected_terms);
+  (float_of_int max_count) /. (float_of_int (Bitv.length selected_terms))
+
+    
+let terms = Array.of_list (Generator.generate 3 Generator.all_ops)
+let _ = Printf.printf "%d\n" (Array.length terms)
+let selected = Bitv.create (Array.length terms) true
+let disc_value = 0x0000000000000001L
+let _ = Printf.printf "%f\n" (refinement_measure terms selected disc_value)
